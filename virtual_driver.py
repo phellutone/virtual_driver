ad4fa525ca526114f8876ebe6e5f60ac4bdcbd1d
phellutone <phellutone@gmail.com>
2021-12-23 08:02:04 +0900

    Create virtual_driver.py

---
+++ virtual_driver.py
@@ -0,0 +1,11 @@
+import bpy
+
+classes = ()
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
