bd5b3d79f038680fed159e260379fbbfec6eb9df
phellutone <phellutone@gmail.com>
2022-01-07 14:14:15 +0900

    Update files

--- virtual_driver.py
+++ virtual_driver.py
@@ -1,12 +1,77 @@
 
+from typing import Literal
 import bpy
 from . import property_tracer
-from .properties import VirtualDriver, InternalVirtualDriver, VirtualDriverIndex
-from .panels import OBJECT_PT_VirtualDriver, OBJECT_UL_VirtualDriver
+
+
+class VirtualDriver(property_tracer.PropertyTracer):
+    identifier: Literal['virtual_driver'] = 'virtual_driver'
+    mute: bpy.props.BoolProperty()
+
+class InternalVirtualDriver(property_tracer.InternalPropTrace):
+    identifier: Literal['internal_virtual_driver'] = 'internal_virtual_driver'
+    mute: bpy.props.BoolProperty()
+
+class VirtualDriverIndex(property_tracer.InternalPropTraceIndex):
+    identifier: Literal['active_virtual_driver_index'] = 'active_virtual_driver_index'
+
 
 property_tracer.PropertyTracer.identifier = VirtualDriver.identifier
 property_tracer.InternalPropTrace.identifier = InternalVirtualDriver.identifier
 property_tracer.InternalPropTraceIndex.identifier = VirtualDriverIndex.identifier
+
+
+
+class OBJECT_UL_VirtualDriver(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            row = layout.row(align=True)
+            row.prop(item, 'name', icon='ANIM', text='', emboss=False)
+            row.prop(item, 'mute', icon='CHECKBOX_HLT' if not item.mute else 'CHECKBOX_DEHLT', text='', emboss=False)
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text='', icon=icon)
+
+class OBJECT_PT_VirtualDriver(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+    bl_idname = 'VIEW3D_PT_virtual_driever'
+    bl_label = 'Virtual Driver'
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        base = property_tracer.prop_trace_base_access_context_check(context)
+        if base is None:
+            return
+        pt: VirtualDriver = getattr(base, VirtualDriver.identifier)
+        ipt: list[InternalVirtualDriver] = getattr(base, InternalVirtualDriver.identifier)
+        index: int = getattr(base, VirtualDriverIndex.identifier)
+
+        layout.template_list(
+            OBJECT_UL_VirtualDriver.__name__,
+            '',
+            base,
+            InternalVirtualDriver.identifier,
+            base,
+            VirtualDriverIndex.identifier,
+            rows=3
+        )
+
+        if not ipt or index < 0:
+            return
+
+        block = ipt[index]
+
+        box = layout.box().column()
+        box.template_any_ID(pt, 'id', 'id_type', text='Prop:')
+        box.template_path_builder(pt, 'data_path', pt.id, text='Path')
+
+        if pt.is_valid:
+            box.prop(pt, 'prop')
+
+
 
 def virtual_driver_base_access_context(context: bpy.types.Context):
     if isinstance(context, bpy.types.Context):
